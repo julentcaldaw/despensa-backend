7ded750126af1f516eea9ca5abc6368bda20ae85
julentcal <juliaentrenas@gmail.com>
2026-02-24 14:32:25 +0100

    act cors

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,8 +1,13 @@
 import jwt from 'jsonwebtoken';
 
 export const authenticateToken = (req, res, next) => {
+  let token;
   const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
+  if (authHeader && authHeader.startsWith('Bearer ')) {
+    token = authHeader.split(' ')[1];
+  } else if (req.cookies && req.cookies.token) {
+    token = req.cookies.token;
+  }
   if (!token) return res.status(401).json({ error: 'Token requerido' });
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return res.status(403).json({ error: 'Token inválido' });
