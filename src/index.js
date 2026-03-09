40a1d8fb54c65c235e42025659e5bd7f9d7fef4c
julentcal <juliaentrenas@gmail.com>
2026-03-09 13:10:59 +0100

    modificación index error render

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv';
-dotenv.config();
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config({ path: '.env.local' });
+}
 console.log('DEBUG JWT_SECRET:', process.env.JWT_SECRET);
 
 import express from 'express';
